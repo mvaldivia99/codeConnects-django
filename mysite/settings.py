1fc78bd85210c1e1793c86e8d7b83c5ae82b25f1
mvaldivia99 <michellvaldivia1@gmail.com>
2021-10-09 10:14:34 -0700

    Added new host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cnbzij8fdb%65v-dlw-)5nwt#ss-%@-(-w^6@q8(4$@x9itsvd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.herokuapp.com']
 
 
 # Application definition
